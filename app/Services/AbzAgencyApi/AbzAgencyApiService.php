165d8026f0724575d57e381d3104d6c06ddd03c4
Bogdan Mamontov <bohdan.mamontov@dotsplatform.com>
2025-01-10 12:56:14 +0200

    Remove annotations

--- app/Services/AbzAgencyApi/AbzAgencyApiService.php
+++ app/Services/AbzAgencyApi/AbzAgencyApiService.php
@@ -1,9 +1,4 @@
 <?php
-/**
- * Description of AbzAgencyService.php
- * @copyright Copyright (c) DOTSPLATFORM, LLC
- * @author    Bogdan Mamontov <bohdan.mamontov@dotsplatform.com>
- */
 
 namespace App\Services\AbzAgencyApi;
 
--- app/Services/AbzAgencyApi/DTO/SearchUsersDTO.php
+++ app/Services/AbzAgencyApi/DTO/SearchUsersDTO.php
@@ -1,9 +1,4 @@
 <?php
-/**
- * Description of SearchUsersDTO.php
- * @copyright Copyright (c) DOTSPLATFORM, LLC
- * @author    Bogdan Mamontov <bohdan.mamontov@dotsplatform.com>
- */
 
 namespace App\Services\AbzAgencyApi\DTO;
 
